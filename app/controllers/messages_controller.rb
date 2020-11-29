00313b48f6cdcedc123ca415890c1cacd44e917f
Sy Rashid <sy.rashid.11@gmail.com>
2020-11-29 23:55:25 +0100

    FEAT: Set up basic messaging ability between users in chatrooms

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,23 @@
+class MessagesController < ApplicationController
+  def create
+    # Find the chatroom for the message
+    @chatroom = Chatroom.find(params[:chatroom_id])
+    # Create the message instance
+    @message = Message.new(message_params)
+    # Associate message with chatroom
+    @message.chatroom = @chatroom
+    @message.user = current_user
+    # Save the message
+    if @message.save
+      redirect_to chatroom_path(@chatroom, anchor: "message-#{@message.id}")
+    else
+      render 'chatroom/show'
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content)
+  end
+end
